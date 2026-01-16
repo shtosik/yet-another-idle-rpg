77475f5b18b2baa801b03eb76140e990cbbfda6b
shtos <marek.d.stankiewicz@gmail.com>
2026-01-16 21:47:46 +0100

    removed unused variables

--- src/app/components/game/battle/battle.component.ts
+++ src/app/components/game/battle/battle.component.ts
@@ -3,47 +3,44 @@ import { ChangeDetectionStrategy, Component, effect, inject, signal, viewChild }
 import { UrlPipe } from 'app/pipes/url.pipe'
 import { SpinnerComponent } from 'app/components/shared/spinner/spinner.component'
 import { BattleStore } from '../../../store/battle/battle.store'
-import ZONES_DATA from '../../../../data/zones-data'
 import { BattleNavBarComponent } from './battle-nav-bar/battle-nav-bar.component'
 import { EnemyComponent } from './enemy/enemy.component'
 import { DamagePopupComponent } from '../damage-popup/damage-popup.component'
 import { BattleFooterComponent } from './battle-footer/battle-footer.component'
 
 @Component({
-    selector: 'app-battle',
-    templateUrl: 'battle.component.html',
-    styleUrls: ['./battle.component.sass'],
-    imports: [
-        CommonModule, UrlPipe, SpinnerComponent,
-        BattleNavBarComponent, EnemyComponent, DamagePopupComponent, BattleFooterComponent,
-    ],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-battle',
+  templateUrl: 'battle.component.html',
+  styleUrls: ['./battle.component.sass'],
+  imports: [
+    CommonModule, UrlPipe, SpinnerComponent,
+    BattleNavBarComponent, EnemyComponent, DamagePopupComponent, BattleFooterComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class BattleComponent {
-    battleStore = inject(BattleStore)
-    enemyContainerContainer = viewChild<EnemyComponent>('enemyElement')
+  battleStore = inject(BattleStore)
+  enemyContainerContainer = viewChild<EnemyComponent>('enemyElement')
 
-    currentZoneId = this.battleStore.currentZoneId
-    isInCombat = this.battleStore.isInCombat
-    currentZoneData = this.battleStore.currentZoneData
+  isInCombat = this.battleStore.isInCombat
+  currentZoneData = this.battleStore.currentZoneData
 
-    enemyPosition = signal({ x: 0, y: 0 })
-    protected readonly ZONES_DATA = ZONES_DATA
+  enemyPosition = signal({ x: 0, y: 0 })
 
-    constructor() {
-        effect(() => {
-            const enemy = this.enemyContainerContainer()
-            const el = enemy?.getEnemyNativeElement()
+  constructor() {
+    effect(() => {
+      const enemy = this.enemyContainerContainer()
+      const el = enemy?.getEnemyNativeElement()
 
-            if (!el) return
+      if (!el) return
 
-            const rect = el.getBoundingClientRect()
+      const rect = el.getBoundingClientRect()
 
-            this.enemyPosition.set({
-                x: rect.left + window.scrollX + rect.width + 10,
-                y: rect.top + window.scrollY + rect.height / 2,
-            })
-        })
-    }
+      this.enemyPosition.set({
+        x: rect.left + window.scrollX + rect.width + 10,
+        y: rect.top + window.scrollY + rect.height / 2,
+      })
+    })
+  }
 }
